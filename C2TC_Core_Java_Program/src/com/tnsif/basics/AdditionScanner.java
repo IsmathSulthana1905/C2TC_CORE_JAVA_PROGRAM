526539c420c4b0c955988f7ff21e15d7689cd992
IsmathSulthana1905 <ismathsulthanah.ece@gmail.com>
2025-09-30 20:46:58 +0530

    basic java program

--- C2TC_Core_Java_Program/src/com/tnsif/basics/AdditionScanner.java
+++ C2TC_Core_Java_Program/src/com/tnsif/basics/AdditionScanner.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class AdditionScanner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner s = new Scanner(System.in);
-		System.out.println("Enter the numbers");
-		int a = s.nextInt();
-		int b = s.nextInt();
-		int c = a+b;
-		System.out.println("Addition of a="+a+" and b="+b+" is" +c);
+		try(Scanner s = new Scanner(System.in)){
+			System.out.println("Enter the numbers");
+			int a = s.nextInt();
+			int b = s.nextInt();
+			int c = a+b;
+			System.out.println("Addition of a="+a+" and b="+b+" is" +c);
+		}
 	}
 }
