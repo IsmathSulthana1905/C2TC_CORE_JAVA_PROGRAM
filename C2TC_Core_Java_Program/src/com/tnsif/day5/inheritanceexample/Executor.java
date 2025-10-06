27689b49bc131ac31c17afa10067a247548c65eb
IsmathSulthana1905 <ismathsulthanah.ece@gmail.com>
2025-10-06 19:48:12 +0530

    this is my basic java program project

---
+++ C2TC_Core_Java_Program/src/com/tnsif/day5/inheritanceexample/Executor.java
@@ -0,0 +1,14 @@
+package com.tnsif.day5.inheritanceexample;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		Student s= new Student();
+		s.setCollegeName("IFET");
+		s.setLocation("Villupuram");
+		s.setSid (34);
+		s.setCourse ("Core Java");
+		s.setSname ("Ismath");
+		System.out.println(s);
+	}
+}
---
+++ C2TC_Core_Java_Program/src/com/tnsif/day5/inheritanceexample/Student.java
@@ -0,0 +1,34 @@
+package com.tnsif.day5.inheritanceexample;
+
+public class Student extends College {
+	private int sid;
+	private String sname;
+	private String course;
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", sname=" + sname + ", course=" + course + ", getCollegeName()="
+				+ getCollegeName() + ", getLocation()=" + getLocation() + "]";
+	}
+	
+	
+	
+
+}
